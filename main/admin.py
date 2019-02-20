b575c3044f769021ee0e71e3975cbf823e09fb27
Shrikant Gupta <shrikantgupta@1MG-LAP-074.local>
2019-02-20 15:18:59 +0530

    Initial Commit

---
+++ main/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Tutorial
+
+# Register your models here.
+
+
+class TutorialAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        ("Title/Date", {"fields": ["tutorial_title", "tutorial_published"]}),
+        ("Content", {"fields": ["tutorial_content"]})
+    ]
+
+
+admin.site.register(Tutorial, TutorialAdmin)

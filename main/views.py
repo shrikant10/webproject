8aff8c8275e14788f8fb7f40152558096c0591a9
Shrikant Gupta <shrikantgupta@1MG-LAP-074.local>
2019-02-20 18:00:48 +0530

    HTML Added in Home Page

--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from .models import Tutorial
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def homepage(request):
-    return HttpResponse("Hello this is <strong> Homepage</strong>.")
+    # return HttpResponse("Hello this is <strong> Homepage</strong>.")
+    return render(request= request, template_name="main/home.html", context={"tutorials": Tutorial.objects.all})
 
 

9388906f8cb5d410d56781f2c8dcb7e24ab9cd4e
Shrikant Gupta <shrikantgupta@1MG-LAP-074.local>
2019-02-20 16:42:33 +0530

    Editor Widget added in Content

--- main/models.py
+++ main/models.py
@@ -8,7 +8,7 @@ class Tutorial(models.Model):
 
     tutorial_title = models.CharField(max_length=200)
     tutorial_content = models.TextField()
-    tutorial_published = models.DateTimeField("date published", default=timezone.now())
+    tutorial_published = models.DateTimeField("date published", default=timezone.now)
 
     def __str__(self):
         return self.tutorial_title
